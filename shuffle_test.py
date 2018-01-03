c2020aeef5f16b26050bbc520c2409feb422549b
Aaron Gallagher <_@habnab.it>
2018-01-02 18:59:31 -0800

    Okay that looks like a good album shuffle.

--- shuffle_test.py
+++ shuffle_test.py
@@ -1,3 +1,4 @@
+import collections
 import random
 import pprint
 
@@ -12,16 +13,16 @@ def build_sets(albums):
     def pick():
         return albums.pop(random.randrange(len(albums)))
 
-    cur = {pick(), pick()}
-    while cur:
-        ret.append(frozenset(cur))
+    cur = set()
+    while albums or cur:
         add_item = (
             albums and
-            (len(cur) == 1 or random.randrange(n_albums) > len(cur)))
+            (len(cur) <= 1 or random.randrange(n_albums) > len(cur)))
         if add_item:
             cur.add(pick())
         else:
             cur.remove(random.sample(cur, 1)[0])
+        ret.append(frozenset(cur))
 
     return ret
 
@@ -36,11 +37,36 @@ class Step(object):
 def stepped_difference(sets):
     return [
         Step(added=b - a, removed=a - b, current=b)
-        for a, b in zip([frozenset()] + sets, sets + [frozenset()])
+        for a, b in zip([frozenset()] + sets, sets)
     ]
 
 
-_albums = range(1, 6)
-_sets = build_sets(_albums)
-pprint.pprint(_sets)
-pprint.pprint(stepped_difference(_sets))
+def shuffle(album_dict):
+    diffs = stepped_difference(build_sets(album_dict))
+    pprint.pprint(diffs)
+    ret = []
+    pool = collections.Counter()
+
+    def pick():
+        album = random.choice(list(pool.elements()))
+        ret.append(album_dict[album].pop(0))
+        pool[album] -= 1
+
+    for diff in diffs:
+        for album in diff.added:
+            pool[album] = len(album_dict[album])
+        for album in diff.removed:
+            print album
+            while pool[album] > 0:
+                pick()
+            print pool
+
+    assert sum(pool.values()) == 0
+    return ret
+
+
+_albums = {
+    n: [(n, m) for m in xrange(random.randint(5, 15))]
+    for n in range(1, 6)
+}
+pprint.pprint(shuffle(_albums))
